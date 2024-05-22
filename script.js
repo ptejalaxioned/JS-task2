b8c1ba17287105ed2bddf8cb182e581a70f49128
ptejalaxioned <tejalp@axioned.com>
2024-05-22 17:11:25 +0530

    JS File commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function changeWidth(element) {
+    if(element.style.width=="8%")
+        {
+            element.style.width="100%";
+        }
+         
+    else
+        {
+            element.style.width="8%";
+        }
+
+}
+
